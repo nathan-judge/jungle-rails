b1051297cb68e5571e733725e3667a8906d67400
nathan-judge <nathanjudge27@gmail.com>
2021-11-10 20:35:28 +0000

    user auth updated

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,12 @@ Rails.application.routes.draw do
     post   :add_item
     post   :remove_item
   end
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
   resources :orders, only: [:create, :show]
 
   namespace :admin do
